eda258ef748d0faaeee4ddd459b07a76d095bfdf
MrGorian <julio.galindo.calzadas@gmail.com>
2024-04-09 23:11:16 -0600

    subir primera version

---
+++ pregunton.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hola, soy un script que pregunta cosas"
+echo "¿Cual es la poblacion de Mexico?"
+grep "Mexico" paises.txt | cut -d"," -f2
+echo "¿Cual es el clima de Mexico?"
+grep "Mexico" paises.txt | cut -d"," -f3
+
+
